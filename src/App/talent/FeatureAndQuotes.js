50d4fed59726b118eff59dc0aaf737ef229923ec
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-03-08 16:07:46 +0330

    Finish talent quotes and pics

--- src/App/talent/FeatureAndQuotes.js
+++ src/App/talent/FeatureAndQuotes.js
@@ -6,8 +6,8 @@ import SharedFeatureAndQuotes from '../../shared/FeatureAndQuotes'
 // Images
 import featuresBg from '../../static/graphics/curved-bg-lightblue.svg'
 import photo1 from '../../static/images/peter_ringelmann.jpg'
-import photo2 from '../../static/images/tanja-heffner.jpg'
-import photo3 from '../../static/images/janko-ferlic.jpg'
+import photo2 from '../../static/images/emil.jpg'
+import photo3 from '../../static/images/shuhei.jpg'
 
 const IndexPage = () => (
   <SharedFeatureAndQuotes
@@ -32,19 +32,22 @@ const featuresAndQuotes = [
     photoUrl: photo1,
   },
   {
+    smallQuote: true,
     reverse: true,
     title: 'Top Jobs for Top Tech Talent',
     desc:
       'Be part of the top 10% and connect with Europe’s top tech companies.',
-    quote: `After only two weeks I received 12 interview invites from a great selection of companies.`,
-    quotee: 'Name Surname',
+    quote: `After only two weeks I received 13 interview invites from a great selection of companies. From these I could choose the cones that best suited me and wound up finding the perfect startup job.`,
+    quotee: 'Emil Ahlbäck, Senior Frontend Developer',
     photoUrl: photo2,
   },
   {
+    smallQuote: true,
     title: 'Know your worth!',
     desc: `Companies apply directly to you so you can compare offers side by side. No more job applications or clumsy back and forths with headhunters.`,
-    quote: 'Honeypot helped me find my dream job in my dream country',
-    quotee: 'Name Surname',
+    quote:
+      "I like Honeypot because companies offer interviews with minimum salary. It saved the effort of applying for jobs, doing interviews, getting offers, and finally finding out that the salary didn't meet my requirements",
+    quotee: 'Shuhei Kagawa, Frontend at Zalando',
     photoUrl: photo3,
   },
 ]
