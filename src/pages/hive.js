1dfcf532435d5dc73bdf1c59c6946f7b031344ee
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-08-03 17:44:16 +0430

    Add Speaker to index

--- src/pages/hive.js
+++ src/pages/hive.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Provider } from 'unstated'
 
 import Header from '../App/hive/Header'
+import Speakers from '../App/hive/Speakers'
 
 export default () => {
   return (
     <Provider>
       <div>
         <Header />
+        <Speakers />
       </div>
     </Provider>
   )
