944dad85522bf30ce80dfbf0a010388d8e173549
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-08-06 21:52:43 +0430

    Add fontSize & sidePadding

--- src/App/hive/Buttons/TicketButton.js
+++ src/App/hive/Buttons/TicketButton.js
@@ -3,11 +3,18 @@ import React from 'react'
 // Local
 import Button from './'
 
-const TicketButton = ({ color = 'yellow', ...props }) => (
+const TicketButton = ({
+  color = 'yellow',
+  fontSize,
+  sidePadding,
+  ...props
+}) => (
   <Button
     href="https://hiveconference2018.eventbrite.ie"
     color={color}
     normalPadding
+    fontSize={fontSize}
+    sidePadding={sidePadding}
     {...props}
   >
     Get Tickets
