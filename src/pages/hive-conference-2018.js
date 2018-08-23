a926f84663f3441f3992f28a5c441467d7fa1f61
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-08-24 01:33:17 +0430

    Add two script tags

--- src/pages/hive-conference-2018.js
+++ src/pages/hive-conference-2018.js
@@ -13,6 +13,7 @@ import About from '../App/hive/About'
 import Footer from '../App/hive/Footer'
 import hiveLogo from '../static/logos/hive-logo-only.png'
 
+const gtmCode = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-PJX579');`
 export default () => {
   const pageTitle = 'Hive Conference by Honeypot | Hire. Scale. Develop.'
   const description =
@@ -48,6 +49,8 @@ export default () => {
         <Sponsor />
         <About />
         <Footer />
+
+        <script async dangerouslySetInnerHTML={{ __html: gtmCode }} />
       </div>
     </Provider>
   )
